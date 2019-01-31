558711403a7a0a837b926c53565ff6aa49aaf201
rrdenney <35283994+rrdenney@users.noreply.github.com>
2019-01-30 19:13:24 -0500

    Small update
    Cleaned up initial page.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Test</p>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Front end.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
